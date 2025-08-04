27a3d5b67ef14d11fd8f9163bf2732aec439c623
Edison <ronquillo5@hotmail.com>
2025-08-04 13:15:52 +0200

    feat(infinite-scroll)
    Infinite scroll donde using custom react hook

--- src/features/movies/components/MovieCard.tsx
+++ src/features/movies/components/MovieCard.tsx
@@ -1,25 +1,36 @@
 import { motion } from "framer-motion";
-import type { Movie, MovieCardProps } from "../../../interfaces/interfaces";
+import type { Movie } from "../../../interfaces/interfaces";
 
-export default function MovieCard({ movie }: MovieCardProps) {
-  const imgUrl = movie.poster_path
-    ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
-    : "https://via.placeholder.com/500x750?text=No+Image";
+interface Props {
+  movie: Movie;
+}
+
+export default function MovieCard({ movie }: Props) {
+  const imgUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
 
   return (
     <motion.div
-      className="rounded-xl overflow-hidden bg-[#F2F2F2] cursor-pointer"
-      initial={{ opacity: 0, boxShadow: "0px 2px 8px rgba(0,0,0,0.2)"}}
+      className="relative rounded-xl overflow-hidden bg-[#F2F2F2] cursor-pointer"
+      initial={{ opacity: 0, boxShadow: "0px 2px 8px rgba(0,0,0,0.2)" }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0, y: 200 }}
-      whileHover={{ scale: 1.02, boxShadow: "0px 10px 24px rgba(0,0,0,0.3)", }}
+      whileHover={{
+        scale: 1.03,
+        boxShadow: "0px 8px 24px rgba(0,0,0,0.3)",
+      }}
       transition={{ duration: 0.3 }}
     >
       <img src={imgUrl} alt={movie.title} className="w-full object-cover" />
-      {/*<div className="p-3">
-        <h3 className="text-black text-sm font-bold truncate">{movie.title}</h3>
-        <p className="text-gray-400 text-xs">{movie.release_date}</p>
-      </div>*/}
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileHover={{ opacity: 1 }}
+        transition={{ duration: 0.3 }}
+        className="absolute inset-0 bg-black/20 flex items-center justify-center p-2"
+      >
+        <p className="text-white text-center font-bold text-sm sm:text-base">
+          {movie.title}
+        </p>
+      </motion.div>
     </motion.div>
   );
 }
--- src/features/movies/components/MoviesGrid.tsx
+++ src/features/movies/components/MoviesGrid.tsx
@@ -1,26 +1,22 @@
-import { useEffect, useState } from "react";
+import { useInfiniteScroll } from "../../../hooks/useInfiniteScroll";
 import { getPopularMovies } from "../../../services/tmdb";
 import type { Movie } from "../../../interfaces/interfaces";
 import MovieCard from "./MovieCard";
 
 export default function MoviesGrid() {
-  const [movies, setMovies] = useState<Movie[]>([]);
-
-  useEffect(() => {
-    async function fetchMovies() {
-      const data = await getPopularMovies(1); // Por ahora solo página 1
-      setMovies(data);
-    }
-    fetchMovies();
-  }, []);
+  const { items: movies, loaderRef, loading } = useInfiniteScroll<Movie>(getPopularMovies);
 
   return (
     <div className="p-6">
-      <h2 className="text-center text-black text-8xl font-bold mb-16">Popular Movies</h2>
+      <h2 className="text-black text-7xl text-center font-extrabold mb-8">Popular Movies</h2>
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
         {movies.map((movie) => (
           <MovieCard key={movie.id} movie={movie} />
         ))}
+      </div>
+
+      <div ref={loaderRef} className="flex justify-center p-4">
+        {loading && <p className="text-gray-400">Loading...</p>}
       </div>
     </div>
   );
